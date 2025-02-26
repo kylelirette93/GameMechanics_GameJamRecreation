64a7addb31c6d331e61c080e84dc7fed68aba6d9
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-02-25 22:18:45 -0400

    Added a few managers and music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip introMusic;
+    public AudioClip gameplayMusic;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+}
